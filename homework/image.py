a3249ad300cc0897d3cf566b8a629e47913316ca
212328057 <meiranpeng@ge.com>
2018-03-19 09:17:25 +0800

    homework q1 and q2

---
+++ homework/image.py
@@ -0,0 +1,16 @@
+import sys
+from PIL import Image
+import numpy as np
+
+img=Image.open('westbrook.jpg')
+
+width  = img.size[0]
+height = img.size[1]
+
+output_img = Image.new('RGB', img.size, (255, 255, 255))
+input_array = np.array(img)
+
+output_array = np.array(input_array/2,dtype=np.uint8)
+print(type(output_array))
+output_img = Image.fromarray(output_array)
+output_img.save('q2.jpg', 'jpeg')
